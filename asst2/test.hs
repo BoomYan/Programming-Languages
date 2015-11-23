f896ac434ed847306d39b6dfa661f940664f76a5
BoomYan <jiamingyan@hotmail.com>
2015-11-22 22:28:31 -0500

    adding parse

--- asst2/test.hs
+++ asst2/test.hs
@@ -1,9 +1,6 @@
 module Main where
 import Data.List
 import Data.Char
-import Data.Bool
-import Data.Either
-import Control.Exception
 
 deleteN :: [Char] -> Int -> [Char]
 deleteN xs n = do    
@@ -18,7 +15,7 @@ eraseSpace input n
 
 list = [1, 1, 1, 1]
 
-getNth :: (Int) -> (Integer)
+getNth :: Int -> Integer
 getNth n = (getNthList list n) !! n
 
 getNthList :: [Integer] -> (Int) -> [Integer]
@@ -29,20 +26,35 @@ getNthList xs n
                      getNthList (xs ++ [(xs !! (size-1) + xs !! (size-2))* xs !! (size-3) `div` xs !! (size-4)]) n
 
 
-getSum :: (Int) -> (Integer)
+getSum :: Int -> Integer
 getSum n = sum (getNthList list n)
 
-getBounds :: (Double) -> [Integer]
+getBounds :: Double -> [Integer]
 getBounds number = do
     let n = getNEqualANumber 0 number
     if ((fromIntegral (getNth n)) == number) then [getNth (n - 1), getNth (n + 1)]
         else [getNth (n - 1), getNth n]
 
 --getFirstNthBiggerOrEqualANumber
-getNEqualANumber :: (Int) -> (Double) -> (Int)
+getNEqualANumber :: Int -> Double -> Int
 getNEqualANumber n number
     | (fromIntegral (getNth n)) >= number = n
     | otherwise = getNEqualANumber (n + 1) number
+
+parseDouble :: [Char] -> Double
+parseDouble xs = do
+    let args = reads xs :: [(Double, String)]
+    case args :: [(Double, String)] of 
+        [(num, "")] -> num
+        [(num, ".")] -> num
+        otherwise -> -1
+
+parseInt :: [Char] -> Int
+parseInt xs = do
+    let args = reads xs :: [(Int, String)]
+    case args :: [(Int, String)] of 
+        [(num, "")] -> num
+        otherwise -> -1
 
 main = do 
     rawInput <- getLine
@@ -58,20 +70,17 @@ main = do
             else do
             -- length command == 2
                 if (command !! 0 == "BOUNDS") then do
-                    number <- try(evaluate (read (command !! 1) :: Double)) :: IO (Either SomeException Double)
-                    case number of
-                        Left ex -> do putStrLn"ERR"
-                        Right val -> do let num = read(command !! 1) :: Double
-                                        let answer = getBounds num
-                                        putStrLn(show (answer !! 0))
-                                        putStrLn(show (answer !! 1))
-                                        main
-                else do    
-                    number <- try(evaluate (read (command !! 1) :: Int)) :: IO (Either SomeException Int)
-                    case number of 
-                        Left ex -> do putStrLn"ERR"
-                        Right val -> do
-                            let num = read(command !! 1) :: Int
+                    let num = parseDouble (command !! 1)
+                    if (num <= 1) then do putStrLn"ERR"
+                        else do
+                            let answer = getBounds num
+                            putStrLn(show (answer !! 0))
+                            putStrLn(show (answer !! 1))
+                            main
+                else do
+                    let num = parseInt (command !! 1)
+                    if (num < 0) then do putStrLn"ERR"
+                        else do
                             case command !! 0 of
                                 "NTH" ->  do let answer = getNth num
                                              putStrLn(show answer)
